d493896d6c101fc87550df39306bda355252d1a2
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-06-03 23:50:44 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,54 @@
+//Array of objects
+const quiz = [
+    {
+        q:"Bye! See you ....... Friday.",
+        options:["on", "in", "at"],
+        answer: 0
+    },
+    {
+        q:"Where were you ....... 28 February?",
+        options:["at", "on", "in"],
+        answer: 1
+    },
+    {
+        q:"I got up ....... 8 o’clock yesterday.",
+        options:["in", "on", "at"],
+        answer: 2
+    },
+    {
+        q:"I like getting up early ....... the morning.",
+        options:["in", "on", "at" ],
+        answer: 0
+    },
+    {
+        q:"My sister got married ....... May.",
+        options:["at", "in", "on"],
+        answer: 1
+    },
+    {
+        q:"Jessica and I first met ....... 2006.",
+        options:["on", "at", "in"],
+        answer: 2
+    },
+    {
+        q:"Did you go out ....... Tuesday?",
+        options:["on", "in", "at"],
+        answer: 0   
+    },
+    {
+        q:"Did you go out ....... Tuesday evening?",
+        options:["at", "on", "in"],
+        answer: 1
+    },
+    {
+        q:"Do you often go out ....... the evening?",
+        options:["at", "on", "in"],
+        answer: 2
+    },
+    {
+        q:"Let’s meet ....... 7.30 tomorrow evening?",
+        options:["at", "on", "in"],
+        answer: 0
+    }
+
+]
